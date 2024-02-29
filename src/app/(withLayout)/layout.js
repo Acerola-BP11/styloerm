2fabe85b95d362c24616b6294b97cd2dae3ab7c3
Acerola-BP11 <felipegrego23@outlook.com>
2024-02-29 10:47:13 -0300

    speed insights removal

--- src/app/(withLayout)/layout.js
+++ src/app/(withLayout)/layout.js
@@ -1,9 +1,6 @@
 import '@/app/globals.css';
 import AuthContext from '@/components/AuthContext/AuthContext';
 import Header from '@/components/Header';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
 
 export const metadata = {
     title: 'Stylo Vest Eventos',
@@ -15,15 +12,11 @@ export default function RootLayout({ children }) {
     return (
         <html lang="pt-br">
             <body>
-                <Analytics>
-                    <SpeedInsights>
-                        <div className='flex flex-1 flex-col h-screen w-screen'>
-                            <Header />
-                            <AuthContext />
-                            {children}
-                        </div>
-                    </SpeedInsights>
-                </Analytics>
+                <div className='flex flex-1 flex-col h-screen w-screen'>
+                    <Header />
+                    <AuthContext />
+                    {children}
+                </div>
             </body>
         </html>
     )
