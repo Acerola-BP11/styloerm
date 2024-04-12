dbc3e80e7c9a4a2712c68284961a8da2588f558c
Acerola-BP11 <felipegrego23@outlook.com>
2024-04-12 11:58:08 -0300

    fix: pdf

--- src/app/(withLayout)/pedidos/[id]/page.js
+++ src/app/(withLayout)/pedidos/[id]/page.js
@@ -25,7 +25,7 @@ export default function EditClient({ params }) {
     const handlePDF = async _ => {
         alert('Entrei na HandlePDF')
         const response = await axios.get(`https://https://styloapi.vercel.app/orders/${params.id}`)
-        console.log(response.data)
+        alert(await response.data)
     }
 
     const onInvalid = (errors) => console.error(errors)
